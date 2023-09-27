a4e1e2c3f79a305a3b0f8d74a78e9ee8cc163fc0
Rizd <rizdhan2002@gmail.com>
2023-09-27 23:38:44 +0700

    Add files via upload

---
+++ lat_p02.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Pertemuan 2</title>
+</head>
+<body>
+    <table border="1" cellspacing="3">
+        <tr>
+            <td colspan="3" align="center" bgcolor="#ffcb9b">DAFTAR PRODUK</td>
+        </tr>
+        <td rowspan="4" colspan="1" bgcolor="#fffe99" width="200" align="center">
+            <img src="canon.jpg" width="150" height="150">
+        </td>
+        <tr>
+            <td width="80px" bgcolor="#00fe67">Nama Produk</td>
+            <td width="400px" bgcolor="#f266f2">Canon EOS M10 Kit EF-M 15-45mm</td>
+        </tr>
+        <tr>
+            <td bgcolor="#00fe67">Harga</td>
+            <td bgcolor="#f266f2">Rp. 4,899,000</td>
+        </tr>
+        <tr>
+            <td bgcolor="#00fe67">Fitur Produk</td>
+            <td bgcolor="#f266f2">
+                <ul>
+                    <li>Kamera Mirrorless</li>
+                    <li>Efektiifitas Piksel : 18 MP</li>
+                    <li>Tipe Sensor : CMOS DIGIC 6</li>
+                    <li>Layar : 3.0 Inch</li>
+                    <li>Memiliki lampu flash dengan jarak jangkauan hingga 5 meter</li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
